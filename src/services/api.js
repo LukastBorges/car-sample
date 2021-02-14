fa130986cb9debf8e1edd0bd0a357ecadf4fdb82
Lucas Borges <lukasborges@outlook.com>
2021-02-14 16:21:04 -0300

    [009]: adds filter options to getVehicles

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:3002/'
 
-export const getVehicles = async () => {
+export const getVehicles = async (filters) => {
   try {
-    const response = await axios.get(`${BASE_URL}vehicles`)
+    const response = await axios.get(`${BASE_URL}vehicles`, { params: filters })
 
     return response.data
   } catch (error) {
