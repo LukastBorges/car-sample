24be4c9a40ac95df74a9ee9bd63e6bf8bf3fe3b5
Lucas Borges <lukasborges@outlook.com>
2021-02-17 01:12:51 -0300

    [022]: adds lazy loading for code spliting

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,35 +1,38 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core/styles'
-
-import Header from '../Header/Header'
-import Vehicles from '../Vehicles/Vehicles'
-import Reservations from '../Reservations/Reservations'
-import RootProvider from '../../contexts/RootContext'
 
 import Toast from '../../components/Toast/Toast'
 import { theme } from '../../styles/theme'
 
 import './App.scss'
 
+const Header = lazy(() => import('../Header/Header'))
+const Vehicles = lazy(() => import('../Vehicles/Vehicles'))
+const Reservations = lazy(() => import('../Reservations/Reservations'))
+const RootProvider = lazy(() => import('../../contexts/RootContext'))
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <RootProvider>
-        <Router>
-          <Header />
-          <main>
-            <Switch>
-              <Route path="/reservas">
-                <Reservations />
-              </Route>
-              <Route path="/">
-                <Vehicles />
-              </Route>
-            </Switch>
-          </main>
-        </Router>
-        <Toast />
-      </RootProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RootProvider>
+          <Router>
+            <Header />
+            <main>
+              <Switch>
+                <Route path="/reservas">
+                  <Reservations />
+                </Route>
+                <Route path="/">
+                  <Vehicles />
+                </Route>
+              </Switch>
+            </main>
+          </Router>
+          <Toast />
+        </RootProvider>
+      </Suspense>
     </ThemeProvider>
   )
 }
