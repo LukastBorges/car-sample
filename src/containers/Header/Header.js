a08923d5b312c27a480d0559df682fef80cf03b5
Lucas Borges <lukasborges@outlook.com>
2021-02-17 00:00:11 -0300

    [018]: adjusts header style

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -8,8 +9,7 @@ import Login from '../Login/Login'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backdropFilter: 'blur(8px)',
-    backgroundColor: 'rgba(255, 255, 255, 21%)'
+    backgroundColor: 'rgba(0, 152, 74, 0.21)'
   },
   toolbar: {
     zIndex: 1,
@@ -17,11 +17,15 @@ const useStyles = makeStyles((theme) => ({
     gridTemplateColumns: '140px 100px 100px calc(80% - 512px) 60px',
     marginLeft: '20%',
     gap: '16px',
-    [theme.breakpoints.down('xs')]: {
-      gridTemplateColumns: '120px 100px 100px 0px 60px',
+    [theme.breakpoints.down('sm')]: {
+      gridTemplateColumns: '120px 100px 100px calc(100% - 512px) 60px',
       gap: '8px',
       marginLeft: '2%'
     }
+  },
+  logo: {
+    fontStyle: 'italic',
+    color: theme.palette.primary.dark
   },
   link: {
     fontSize: '16px',
@@ -37,17 +41,28 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
   const classes = useStyles()
+  const { pathname } = useLocation()
 
   return (
     <AppBar position="sticky" color="transparent" elevation={0} className={classes.root}>
       <Toolbar className={classes.toolbar}>
         <Typography variant="h4" className={classes.logo}>
-          LocaVel
+          LocAqui
         </Typography>
-        <Link href="/" className={classes.link} color="secondary" underline="none">
+        <Link
+          href="/"
+          className={classes.link}
+          color={pathname === '/' ? 'primary' : 'textPrimary'}
+          underline="none"
+        >
           Veiculos
         </Link>
-        <Link href="/reservas" className={classes.link} color="secondary" underline="none">
+        <Link
+          href="/reservas"
+          className={classes.link}
+          color={pathname === '/reservas' ? 'primary' : 'textPrimary'}
+          underline="none"
+        >
           Reservas
         </Link>
         <aside className={classes.login}>
